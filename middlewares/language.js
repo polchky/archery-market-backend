cf8df182c428e9bf4bf08cd63a47bc70699147e5
Pascal Gremaud <pascal.gremaud@unifr.ch>
2020-01-27 14:08:12 +0100

    Allow language specification in query string

--- middlewares/language.js
+++ middlewares/language.js
@@ -1,14 +1,10 @@
-const compose = require('koa-compose');
-const assert = require('@middlewares/assert');
 const constants = require('@constants');
 
-const language = compose([
-    assert.headers('language'),
-    (ctx, next) => {
-        ctx.assert(constants.languages.indexOf(ctx.request.headers.language) > -1, 400, 'Unsupported language');
-        ctx.language = ctx.request.headers.language;
-        return next();
-    },
-]);
+const language = (ctx, next) => {
+    ctx.language = ctx.request.headers.language || ctx.request.query.language;
+    if (!ctx.language) ctx.throw(400, 'Missing language specifier');
+    ctx.assert(constants.languages.indexOf(ctx.language) > -1, 400, 'Unsupported language');
+    return next();
+};
 
 module.exports = language;
